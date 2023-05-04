f0b1e6168bcf14967f76c58b9985c4720fe2ac3b
naveen <naveennatarajan0097@gmail.com>
2023-05-04 15:24:09 +0530

    cliq coupon

---
+++ coupon-popup.js
@@ -0,0 +1,9 @@
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('content').style.opacity = '0.5'
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('content').style.opacity = '1'
+}
